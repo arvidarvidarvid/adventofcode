9cf1a059dd199fddb251072f7336fa7b64c0d82d
Arvid Olovsson <arvid.olovsson@bonnier.se>
2015-12-12 12:24:00 +0100

    Reduce duplicate code

--- 12/12.py
+++ 12/12.py
@@ -5,37 +5,21 @@ data = open('12.txt', 'r').read()
 data = eval(data)
 
 
-# Part one
-
-def get_leaves(data):
-    if isinstance(data, dict):
-        return sum([get_leaves(v) for k, v in data.items()])
-    elif isinstance(data, list):
-        return sum([get_leaves(i) for i in data])
-    elif isinstance(data, int):
-        return data
-    else:
-        return 0
-
-print 'Part 1: %s' % get_leaves(data)
-
-
-# Part two
-
-def get_leaves_v2(data):
+def get_leaves(data, care_about_red=False):
     if isinstance(data, dict):
         _sum = 0
         for k, v in data.items():
-            if 'red' in (k, v):
-                return 0
-            else:
-                _sum += get_leaves_v2(v)
+            if care_about_red:
+                if 'red' in (k, v):
+                    return 0
+            _sum += get_leaves(v, care_about_red)
         return _sum
     elif isinstance(data, list):
-        return sum([get_leaves_v2(i) for i in data])
+        return sum([get_leaves(i, care_about_red) for i in data])
     elif isinstance(data, int):
         return data
     else:
         return 0
 
-print 'Part 2: %s' % get_leaves_v2(data)
+print 'Part 1: %s' % get_leaves(data, False)
+print 'Part 2: %s' % get_leaves(data, True)
