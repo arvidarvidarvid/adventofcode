9639d9a38890068d064fa9d37a79d200e5212f5c
Arvid Olovsson <arvid.olovsson@bonnier.se>
2015-12-14 19:11:05 +0100

    Nicer names and output

--- day02/2a.py
+++ day02/2a.py
@@ -1,11 +1,8 @@
 with open('2.txt', 'r') as f:
     gifts = f.read()
 
-
 gs = gifts.split('\n')
-
 int_gifts = []
-
 
 for g in gs[1:-1]:
     sides = g.split('x')
@@ -14,14 +11,13 @@ for g in gs[1:-1]:
     h = int(sides[2])
     int_gifts.append(sorted([l, w, h]))
 
-total = 0
-tot_ribbon = 0
+total_paper = 0
+total_ribbon = 0
 
 for g in int_gifts:
+    total_ribbon = total_ribbon + 2*g[0] + 2*g[1] + g[0]*g[1]*g[2]
+    total_paper = (total_paper + 2*g[0]*g[1] + 2*g[1]*g[2] + 2*g[0]*g[2] +
+                   g[0]*g[1])
 
-    tot_ribbon = tot_ribbon + 2*g[0] + 2*g[1] + g[0]*g[1]*g[2]
-
-    total = total + 2*g[0]*g[1] + 2*g[1]*g[2] + 2*g[0]*g[2] + g[0]*g[1]
-
-print total
-print tot_ribbon
+print 'Part 1: %s sqft of paper needed' % total_paper
+print 'Part 2: %s feet of ribbon needed' % total_ribbon
