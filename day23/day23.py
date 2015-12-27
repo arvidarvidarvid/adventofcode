a7e418845c2e49193359cb0d88940fd1934d1bc5
Arvid Olovsson <arvid.olovsson@bonnier.se>
2015-12-27 17:33:56 +0100

    Removed debug prints, reworked result printout

--- day23/day23.py
+++ day23/day23.py
@@ -1,110 +1,86 @@
 instructions = open('day23input.txt', 'r').read().split('\n')
 
-a = 1
-b = 0
 
-"""
-hlf r sets register r to half its current value, then continues with the next instruction.
-tpl r sets register r to triple its current value, then continues with the next instruction.
-inc r increments register r, adding 1 to it, then continues with the next instruction.
-jmp offset is a jump; it continues with the instruction offset away relative to itself.
-jie r, offset is like jmp, but only jumps if register r is even ("jump if even").
-jio r, offset is like jmp, but only jumps if register r is 1 ("jump if one", not odd).
-"""
-
-print instructions
-
-i = 0
-while i < len(instructions):
-    inst = instructions[i].split()
-    action = inst[0]
-    print inst
-    if action == 'hlf':
-        print 'hlf'
-        r = inst[1]
-        if r == 'a':
-            print 'a'
-            a = a/2
-        if r == 'b':
-            print 'b'
-            b = b/2
-        i += 1
-    elif action == 'tpl':
-        print 'tpl'
-        r = inst[1]
-        if r == 'a':
-            a = a*3
-            print 'a'
-        if r == 'b':
-            b = b*3
-            print 'b'
-        i += 1
-    elif action == 'inc':
-        r = inst[1]
-        if r == 'a':
-            a += 1
-        if r == 'b':
-            b += 1
-        i += 1
-    elif action == 'jmp':
-        jmp = inst[1]
-        direction = jmp[0]
-        distance = jmp[1:]
-        if direction == '-':
-            i -= int(distance)
-        if direction == '+':
-            i += int(distance)
-    elif action == 'jie':
-        print 'jie'
-        jmp = inst[2]
-        direction = jmp[0]
-        r = inst[1][0]
-        distance = jmp[1:]
-        jump = False
-        if r == 'a':
-            print 'a'
-            if a % 2 == 0:
-                print 'a: ' + str(a) + ' is even'
-                jump = True
-        if r == 'b':
-            print 'b'
-            if b % 2 == 0:
-                print 'b: ' + str(b) + ' is even'
-                jump = True
-        if jump is True:
+def get_results(a=0, b=0):
+    i = 0
+    while i < len(instructions):
+        inst = instructions[i].split()
+        action = inst[0]
+        if action == 'hlf':
+            r = inst[1]
+            if r == 'a':
+                a = a/2
+            if r == 'b':
+                b = b/2
+            i += 1
+        elif action == 'tpl':
+            r = inst[1]
+            if r == 'a':
+                a = a*3
+            if r == 'b':
+                b = b*3
+            i += 1
+        elif action == 'inc':
+            r = inst[1]
+            if r == 'a':
+                a += 1
+            if r == 'b':
+                b += 1
+            i += 1
+        elif action == 'jmp':
+            jmp = inst[1]
+            direction = jmp[0]
+            distance = jmp[1:]
             if direction == '-':
                 i -= int(distance)
             if direction == '+':
                 i += int(distance)
+        elif action == 'jie':
+            jmp = inst[2]
+            direction = jmp[0]
+            r = inst[1][0]
+            distance = jmp[1:]
+            jump = False
+            if r == 'a':
+                if a % 2 == 0:
+                    jump = True
+            if r == 'b':
+                if b % 2 == 0:
+                    jump = True
+            if jump is True:
+                if direction == '-':
+                    i -= int(distance)
+                if direction == '+':
+                    i += int(distance)
+            else:
+                i += 1
+        elif action == 'jio':
+            jmp = inst[2]
+            direction = jmp[0]
+            r = inst[1][0]
+            distance = jmp[1:]
+            jump = False
+            if r == 'a':
+                if a == 1:
+                    jump = True
+            if r == 'b':
+                if b == 1:
+                    jump = True
+            if jump is True:
+                if direction == '-':
+                    i -= int(distance)
+                if direction == '+':
+                    i += int(distance)
+            else:
+                i += 1
         else:
-            i += 1
-    elif action == 'jio':
-        print 'jio'
-        jmp = inst[2]
-        direction = jmp[0]
-        r = inst[1][0]
-        distance = jmp[1:]
-        jump = False
-        if r == 'a':
-            print 'a'
-            if a == 1:
-                print 'a: ' + str(a) + ' is odd'
-                jump = True
-        if r == 'b':
-            print 'b'
-            if b == 1:
-                print 'b: ' + str(b) + ' is odd'
-                jump = True
-        if jump is True:
-            if direction == '-':
-                i -= int(distance)
-            if direction == '+':
-                i += int(distance)
-        else:
-            i += 1
-    else:
-        print inst
-        break
+            break
+    return (a, b)
 
-print a
-print b
+# Part 1
+print (u'Part 1: The value of b after processing all instructions is %i' %
+       get_results()[1])
+
+# Part 2
+print (u'Part 2: The value of b after processing all instructions is %i' %
+       get_results(1, 0)[1])
