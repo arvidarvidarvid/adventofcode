7eb752da18aa0b5fc7b9cc02f31b471a235a25b8
Arvid Olovsson <arvido@spotify.com>
2017-12-01 10:36:44 +0100

    Day 1 and templates for 2017

---
+++ 2017/day01/day.py
@@ -0,0 +1,37 @@
+import logging
+import tqdm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def get_input():
+    with open('day.input') as file:
+        return file.read()
+
+
+def get_test_input():
+    return '1122'
+
+
+def main():
+
+    input = get_input()
+    input = input
+    double_input = input + input
+
+    sum_1 = 0
+    sum_2 = 0
+
+    for i, val in tqdm(enumerate(input)):
+        if int(val) == int(input[i - 1]):
+            sum_1 += int(input[i - 1])
+        if int(val) == int(double_input[int(i + len(input) / 2)]):
+            sum_2 += int(val)
+
+    logger.info('Result 1: %s' % sum_1)
+    logger.info('Result 2: %s' % sum_2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2017/day02/day.py
@@ -0,0 +1,25 @@
+from copy import deepcopy
+from datetime import datetime, timedelta
+import itertools
+import logging
+import math
+import numpy as np
+import re
+import tqdm
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def get_input():
+    with open('day.input') as file:
+        return file.read()
+
+
+def main():
+    logger.info('Result 1')
+    logger.info('Result 2')
+
+
+if __name__ == '__main__':
+    main()
