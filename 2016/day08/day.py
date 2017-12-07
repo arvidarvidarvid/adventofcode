36cc1eb244514c0435f8c93dfcfb91b18d2eb168
Arvid Olovsson <arvido@spotify.com>
2017-12-07 13:45:59 +0100

    2016 day 8

--- 2016/day08/day.py
+++ 2016/day08/day.py
@@ -1,11 +1,7 @@
 from copy import deepcopy
-from datetime import datetime, timedelta
-import itertools
 import logging
-import math
 import numpy as np
 import re
-import tqdm
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -27,13 +23,10 @@ def get_grid(y, x):
 def apply_instructions(grid, instructions):
     for instruction in instructions:
         grid = apply(grid, instruction)
-        print(grid)
     return grid
 
 
 def apply(grid, instruction):
-
-    print(instruction)
 
     rect_pattern = r'rect (\d+)x(\d+)'
     rotate_pattern = r'rotate (.+) [xy]=(\d+) by (\d+)'
@@ -50,13 +43,17 @@ def apply(grid, instruction):
 
 def rotate(grid, dim, pos, dist):
 
-    if dim == 'row':
-        prev_state = deepcopy(grid[pos])
-        for index, value in grid[pos]:
-            new_index = index +
+    prev_state = deepcopy(grid)
 
+    if dim == 'row':
+        for index, value in enumerate(grid[pos]):
+            new_index = (index + dist) % len(grid[pos])
+            grid[pos][new_index] = prev_state[pos][index]
     elif dim == 'column':
-        pass
+        grid = rotate(grid.transpose(), 'row', pos, dist)
+        grid = grid.transpose()
+
+    return grid
 
 
 def rect(grid, y, x):
@@ -66,6 +63,20 @@ def rect(grid, y, x):
     return grid
 
 
+def count_lit(grid):
+    lit = 0
+    for row in grid:
+        for col in row:
+            lit += col
+    return int(lit)
+
+
+def draw_code(grid):
+    for row in grid:
+        print(''.join([str(int(r)) for r in row]).replace(
+            '0', ' ').replace('1', '#'))
+
+
 def test():
     raw_instructions = """rect 3x2
     rotate column x=1 by 1
@@ -73,8 +84,8 @@ def test():
     rotate column x=1 by 1"""
     instructions = split_instructions(raw_instructions)
     grid = get_grid(3, 7)
-    print(grid)
-    apply_instructions(grid, [instructions[0]])
+    grid = apply_instructions(grid, instructions)
+    assert count_lit(grid) == 6
     logger.info('Tests passed')
 
 
@@ -82,8 +93,12 @@ def main():
 
     input = get_input()
 
-    logger.info('Result 1')
+    grid = get_grid(6, 50)
+    instructions = split_instructions(input)
+    grid = apply_instructions(grid, instructions)
+    logger.info('Result 1: %s' % count_lit(grid))
     logger.info('Result 2')
+    draw_code(grid)
 
 
 if __name__ == '__main__':
