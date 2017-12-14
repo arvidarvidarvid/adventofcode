2b50db805050b7347ded6dcadc242baf1c817337
Arvid Olovsson <arvido@spotify.com>
2017-12-14 10:04:04 +0100

    Faster 14

--- 2017/day14/day.py
+++ 2017/day14/day.py
@@ -23,59 +23,59 @@ def get_hashes(seed, count):
 
 
 def hashes_to_used(hashes):
-    s = 0
-    for h in hashes:
-        for c in h:
-            s += int(c)
-    return s
+    return sum([int(c) for h in hashes for c in h])
 
 
 def pad_hashes(hashes):
-    padded_hash = [''.join(['0' for i in range(0, len(hashes) + 2)])]
+    hashes = [hash.replace('0', '.').replace('1', '#') for hash in hashes]
+    padded_hash = [['.' for i in range(0, len(hashes) + 2)]]
     for i in range(0, len(hashes)):
-        padded_hash.append('0' + hashes[i] + '0')
-    padded_hash.append(''.join(['0' for i in range(0, len(hashes) + 2)]))
+        padded_hash.append(['.'] + [c for c in hashes[i]] + ['.'])
+    padded_hash += [['.' for i in range(0, len(hashes) + 2)]]
     return padded_hash
 
 
 def find_groups(hashes):
     hashes = pad_hashes(hashes)
     groups = []
-    seen_coords = []
+    num_groups = 0
     for y, hash in enumerate(hashes):
         for x, char in enumerate(hash):
-            group_coords = find_related_coords(y, x, seen_coords, hashes)
+            group_coords = find_related_coords(y, x, num_groups, hashes)
             groups.append(group_coords)
-            seen_coords += group_coords
-            seen_coords = list(set(seen_coords))
-    return sum([1 for g in groups if len(g) > 0])
+            if group_coords != []:
+                num_groups += 1
+    return num_groups, hashes
 
 
-def find_related_coords(y, x, seen_coords, hashes):
+def find_related_coords(y, x, group_index, hashes):
 
-    if hashes[y][x] == '0' or (y, x) in seen_coords:
+    if hashes[y][x] != '#':
         return []
 
-    _seen = seen_coords
-    _seen.append((y, x))
+    hashes[y][x] = group_index
     _group_neighbours = [(y, x)]
 
     for offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         _y = y + offset[0]
         _x = x + offset[1]
-        if hashes[_y][_x] == '1':
-            if (_y, _x) not in _seen:
-                _group_neighbours += find_related_coords(_y, _x, _seen, hashes)
-                _seen += _group_neighbours
+        if hashes[_y][_x] == '#':
+            _group_neighbours += find_related_coords(
+                _y, _x, group_index, hashes)
 
     return list(set(_group_neighbours))
+
+
+def print_rect(hashes, size):
+    for hash in hashes[:size]:
+        print(hash[:size])
 
 
 def test():
     test_input = get_input('test.input')
     hashes = get_hashes(test_input, 128)
     assert hashes_to_used(hashes) == 8108
-    groups = find_groups(hashes)
+    groups, hashes = find_groups(hashes)
     assert groups == 1242
     logger.info('Tests passed')
     return True
@@ -87,7 +87,8 @@ def main():
     hashes = get_hashes(input, 128)
 
     logger.info('Result 1: %s' % hashes_to_used(hashes))
-    logger.info('Result 2: %s' % find_groups(hashes))
+    groups, _ = find_groups(hashes)
+    logger.info('Result 2: %s' % groups)
 
 
 if __name__ == '__main__':
