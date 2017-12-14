bbe202a0e13139d8ef6929be5a0601b78191c532
Arvid Olovsson <arvido@spotify.com>
2017-12-14 10:11:25 +0100

    Cleanup

--- 2017/day14/day.py
+++ 2017/day14/day.py
@@ -1,94 +1,59 @@
-import logging
 from knothash import input_to_hex
-
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
 
 
 def get_input(filename='day.input'):
     return open(filename, 'r').read()
 
 
-def hex_hash_to_bits(hexhash):
+def hex_to_bin(hexhash):
     return bin(int(hexhash, 16))[2:].zfill(128)
 
 
-def get_hashes(seed, count):
-    hashes = []
-    for i in range(0, count):
-        input = ''.join([seed, '-', str(i)])
-        hashes.append(hex_hash_to_bits(input_to_hex(input)))
-    return hashes
+def get_hashes(seed, n):
+    hashes = [hex_to_bin(input_to_hex(seed + '-' + str(i)))
+              .replace('1', '#').replace('0', '.') for i in range(0, n)]
+    dot_line = ['.' for i in range(0, len(hashes) + 2)]
+    return [dot_line] + [['.'] + [c for c in hashes[i]] + ['.']
+                         for i in range(len(hashes))] + [dot_line]
 
 
 def hashes_to_used(hashes):
-    return sum([int(c) for h in hashes for c in h])
+    return sum([1 for h in hashes for c in h if c == '#'])
 
 
-def pad_hashes(hashes):
-    hashes = [hash.replace('0', '.').replace('1', '#') for hash in hashes]
-    padded_hash = [['.' for i in range(0, len(hashes) + 2)]]
-    for i in range(0, len(hashes)):
-        padded_hash.append(['.'] + [c for c in hashes[i]] + ['.'])
-    padded_hash += [['.' for i in range(0, len(hashes) + 2)]]
-    return padded_hash
-
-
-def find_groups(hashes):
-    hashes = pad_hashes(hashes)
-    groups = []
+def count_groups(hashes):
     num_groups = 0
     for y, hash in enumerate(hashes):
         for x, char in enumerate(hash):
-            group_coords = find_related_coords(y, x, num_groups, hashes)
-            groups.append(group_coords)
-            if group_coords != []:
-                num_groups += 1
+            num_groups += expand_group(y, x, num_groups, hashes)
     return num_groups, hashes
 
 
-def find_related_coords(y, x, group_index, hashes):
-
+def expand_group(y, x, group_index, hashes):
     if hashes[y][x] != '#':
-        return []
-
+        return 0
     hashes[y][x] = group_index
-    _group_neighbours = [(y, x)]
-
     for offset in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         _y = y + offset[0]
         _x = x + offset[1]
-        if hashes[_y][_x] == '#':
-            _group_neighbours += find_related_coords(
-                _y, _x, group_index, hashes)
-
-    return list(set(_group_neighbours))
-
-
-def print_rect(hashes, size):
-    for hash in hashes[:size]:
-        print(hash[:size])
+        expand_group(_y, _x, group_index, hashes)
+    return 1
 
 
 def test():
     test_input = get_input('test.input')
     hashes = get_hashes(test_input, 128)
     assert hashes_to_used(hashes) == 8108
-    groups, hashes = find_groups(hashes)
-    assert groups == 1242
-    logger.info('Tests passed')
-    return True
+    assert count_groups(hashes)[0] == 1242
+    print('Tests passed')
 
 
 def main():
-
     input = get_input()
     hashes = get_hashes(input, 128)
-
-    logger.info('Result 1: %s' % hashes_to_used(hashes))
-    groups, _ = find_groups(hashes)
-    logger.info('Result 2: %s' % groups)
+    print('Result 1: %s' % hashes_to_used(hashes))
+    groups, _ = count_groups(hashes)
+    print('Result 2: %s' % groups)
 
 
 if __name__ == '__main__':
