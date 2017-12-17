b8f716187a50e4ab673457da32c57f1540697d5e
Arvid Olovsson <arvido@spotify.com>
2017-12-17 12:04:41 +0100

    2017 day 17 without cheat imports

--- 2017/day17/day.py
+++ 2017/day17/day.py
@@ -1,5 +1,3 @@
-from blist import blist
-from tqdm import tqdm
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -7,9 +5,9 @@ logger = logging.getLogger()
 
 
 def step_and_insert(step_size, n_inserts=1):
-    circle = blist([0])
+    circle = [0]
     position = 0
-    for s in tqdm(range(1, n_inserts + 1)):
+    for s in range(1, n_inserts + 1):
         position = (position + step_size) % s
         circle.insert(position + 1, s)
         position += 1
@@ -21,6 +19,19 @@ def val_after_val(circle, after_val):
     return circle[ix + 1]
 
 
+def val_after_val_nonmaterialized():
+    inserts = 50000000
+    step_size = 366
+    position = 0
+    val_after_zero = None
+    for s in range(1, inserts + 1):
+        position = (position + step_size) % s
+        if position == 0:
+            val_after_zero = s
+        position += 1
+    return val_after_zero
+
+
 def test():
     test_input = 3
     circle = step_and_insert(test_input, n_inserts=2017)
@@ -29,13 +40,10 @@ def test():
 
 
 def main():
-
     input = 366
     circle = step_and_insert(input, n_inserts=2017)
     logger.info('Result 1: %s' % val_after_val(circle, 2017))
-
-    circle = step_and_insert(input, n_inserts=50000000)
-    logger.info('Result 2: %s' % val_after_val(circle, 0))
+    logger.info('Result 2: %s' % val_after_val_nonmaterialized())
 
 
 if __name__ == '__main__':
