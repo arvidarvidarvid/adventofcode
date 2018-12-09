0c3b85b65dfaf62a403a9496edde7dc74010848a
Arvid Olovsson <arvido@spotify.com>
2018-12-10 00:13:30 +0100

    Day09 functioning B

--- 2018/day09/day.py
+++ 2018/day09/day.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tqdm
 import logging
 
@@ -6,85 +5,80 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def get_circle_insert_index(length, current_index):
-    if length == 1:
-        return 1
-    else:
-        if current_index + 2 <= length:
-            return current_index + 2
+class Marble(object):
+
+    def __init__(self, value, prev_marble=None, next_marble=None):
+        self.value = value
+        self._prev = prev_marble
+        self._next = next_marble
+
+    def next(self):
+        return self._next
+
+    def prev(self):
+        return self._prev
+
+    def set_prev(self, new):
+        self._prev = new
+
+    def set_next(self, new):
+        self._next = new
+
+
+def print_circle(root_marble, current_marble):
+
+    marble = root_marble
+    values = []
+
+    while True:
+        if marble == current_marble:
+            values.append('({})'.format(marble.value))
         else:
-            if current_index == length-1:
-                return 1
-            else:
-                return 0
-        return -(length - current_index - 2) % (length - 1)
+            values.append('{}'.format(marble.value))
+        if marble.next() == root_marble:
+            break
+        marble = marble.next()
+
+    print(' '.join(values))
 
 
-def remove_marble(circle, length, current_index):
-    remove_index = (current_index - 7) % length
-    removed_marble = circle[remove_index]
-    circle = np.delete(circle, remove_index)
-    return circle, length-1, removed_marble, remove_index
+def play_game(n_players, last_marble_value):
 
+    root_marble = Marble(0)
+    current_marble = root_marble
+    current_marble.set_prev(current_marble)
+    current_marble.set_next(current_marble)
 
-def recenter_circle(circle, length, current_index):
-    zero_index = np.where(circle == 0)[0][0]
-    circle = np.concatenate((circle[zero_index:],
-                             circle[:zero_index]))
-    current_index = (current_index - zero_index) % len(circle)
-    return circle, current_index
-
-
-def print_circle(circle, length, current_index):
-
-    _circle, _current_index = recenter_circle(
-        circle, length, current_index)
-
-    s = ''
-    for ix, marble in enumerate(_circle[:length]):
-        if ix == _current_index:
-            s += ' ({}) '.format(marble)
-        else:
-            s += ' {} '.format(marble)
-    print(s)
-
-
-def play_game(players, last_marble_value):
-
-    circle = np.array([0])
-    length = 1
+    players = [i for i in range(n_players)]
+    scores = [0 for i in range(n_players)]
     rounds = 0
+    next_value = 1
 
-    player_list = [i+1 for i in range(players)]
-    scores = [0 for i in range(players)]
+    for _ in tqdm.tqdm(range(last_marble_value)):
 
-    next_marble = 1
-    current_index = 0
+        # print_circle(root_marble, current_marble)
 
-    for _ in tqdm.tqdm(range(int(last_marble_value))):
-    # for _ in range(int(last_marble_value)):
-        next_player = player_list[rounds % players]
+        player = players[rounds % n_players]
 
-        if next_marble > 0 and next_marble % 23 == 0:
-            circle, length, removed_marble, current_index = remove_marble(
-                circle, length, current_index)
-            score = next_marble + removed_marble
-            scores[next_player-1] += score
+        if next_value % 23 == 0:
+            to_remove = current_marble.prev().prev().prev().prev().prev().prev().prev()
+            score = to_remove.value + next_value
+            scores[player] += score
+            current_marble = to_remove.next()
+            prev = to_remove.prev()
+            prev.set_next(current_marble)
+            current_marble.set_prev(prev)
 
         else:
-            insert_index = get_circle_insert_index(length,
-                                                   current_index)
-            circle = np.insert(circle, insert_index, next_marble)
-            length += 1
-            current_index = insert_index
+            new_marble = Marble(next_value,
+                                current_marble.next(),
+                                current_marble.next().next())
+            new_marble.prev().set_next(new_marble)
+            new_marble.next().set_prev(new_marble)
+            current_marble = new_marble
 
-        next_marble += 1
+        next_value += 1
         rounds += 1
-
-        # print_circle(circle, length, current_index)
-
-    #print('elf ', int(player_list[scores.index(max(scores))]),
-    #      ' wins with ', int(max(scores)), ' points')
 
     return max(scores)
 
@@ -98,7 +92,6 @@ def test():
     assert play_game(21, 6111) == 54718
     assert play_game(30, 5807) == 37305
 
-    # assert p2(test_input_play_game) == 'ABC'
     logger.info('Tests passed')
 
 
